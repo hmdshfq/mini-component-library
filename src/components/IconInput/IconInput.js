733bcfa24012cd9028ea8b5e51c58349b9f6a8e0
Hammad <hmdshfq@outlook.com>
2022-02-09 19:00:33 +0100

    complete icon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -25,18 +25,18 @@ const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
     const styles = STYLES[size];
 
     return (
-        <Wrapper style={{}}>
+        <Wrapper>
             <VisuallyHidden>{label}</VisuallyHidden>
             <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
-                <Icon id={icon} styles={{'--size': styles.iconSize + 'px'}}></Icon>
+                <Icon id={icon} size={styles.iconSize}></Icon>
             </IconWrapper>
             <TextInput
                 {...delegated}
                 style={{
                     '--width': width + 'px',
-                    '--height': styles.height + 'px',
+                    '--height': styles.height / 16 + 'rem',
                     '--border-thickness': styles.borderThickness + 'px',
-                    '--font-size': styles.fontSize + 'px',
+                    '--font-size': styles.fontSize / 16 + 'rem',
                 }}
             />
         </Wrapper>
@@ -64,17 +64,17 @@ const IconWrapper = styled.div`
 const TextInput = styled.input`
     width: var(--width);
     height: var(--height);
+    font-size: var(--font-size);
     border: none;
     border-bottom: var(--border-thickness) solid ${COLORS.black};
     padding-left: var(--height);
     color: inherit;
-    font-size: var(--font-size);
     font-weight: 700;
     outline-offset: 2px;
 
     &::placeholder {
-        color: ${COLORS.gray500};
         font-weight: 400;
+        color: ${COLORS.gray500};
     }
 `;
 
